6493d4f3a493eb804227401a0fa9f8af7e5d3d7f
YonaAncheo <111770436+YonaAncheo@users.noreply.github.com>
2022-08-23 23:22:56 -0400

    Add files via upload
    se agrega ejercicio 6 de la guia

---
+++ GuiaAlgoritmos2/src/clases/Ejercicio_6.java
@@ -0,0 +1,62 @@
+package clases;
+
+import java.util.Scanner;
+
+public class Ejercicio_6 {
+
+//Un profesor necesita conocer qué porcentaje de hombres y el de
+//mujeres que hay en un grupo de estudiantes. Ingresar la cantidad
+//de mujeres y la de hombres existentes en un curso, calcular y
+//mostrar el equivalente en porcentaje.
+    Scanner in = new Scanner(System.in);
+    //entrada solicitada a usuarios será la cantidad de hombres y mujeres.
+    float mujeres, hombres;
+    final int CIEN = 100;
+    float phombre, pmujeres;
+
+    public void SolicitarDatos() {
+        System.out.println("-----------------------------------------------");
+        System.out.println("------------------EJERCICIO6-------------------");
+        System.out.println("-----------------------------------------------");
+        System.out.println("--- Ingrese la cantidad de hombres en el curso: ");
+        hombres = in.nextInt();
+        System.out.println("--- Ingrese la cantidad de mujeres en el curso: ");
+        mujeres = in.nextInt();
+
+    }
+
+    //cantidad total de estudiantes
+    public float EstudiantesTotal() {
+        float total = mujeres + hombres;
+        return total;
+    }
+
+    //metodo para el calculo del pocentajes
+    public void Porcentajes() {
+
+        phombre = (hombres / EstudiantesTotal()) * 100;
+        pmujeres = (mujeres / EstudiantesTotal()) * 100;
+
+    }
+
+    //metodo para mostrar resultados
+    public void MostrarResultados() {
+        System.out.println("-----------------------------------------------");
+        System.out.println("--- El total de estudiantes en el grupo es de " + EstudiantesTotal() + ".");
+        System.out.println("--- El porcentaje de mujeres en el grupo es de " + pmujeres + ".");
+        System.out.println("--- El porcentaje de hombres en el grupo es de " + phombre + ".");
+        System.out.println("===============================================");
+        System.out.println("================FIN DEL PROGRAMA===============");
+        System.out.println("===============================================");
+    }
+    
+    //Metodo main
+    public static void main(String[] args) {
+        Ejercicio_6 ejercicio = new Ejercicio_6();
+        ejercicio.SolicitarDatos();
+        ejercicio.EstudiantesTotal();
+        ejercicio.Porcentajes();
+        ejercicio.MostrarResultados();
+    }
+
+}
